d74a98946f8190e60e74cbc6322167c306df6cbf
Johnny Shields <johnny.shields@gmail.com>
2013-07-12 16:21:16 +0900

    Has one Hash should default to {}, analogous to has many Array defaulting to [].

--- lib/mongoid/alize/callbacks/from/one.rb
+++ lib/mongoid/alize/callbacks/from/one.rb
@@ -30,7 +30,7 @@ module Mongoid
           def define_fields
             ensure_field_not_defined!(prefixed_name, klass)
             klass.class_eval <<-CALLBACK, __FILE__, __LINE__ + 1
-              field :#{prefixed_name}, :type => Hash, :default => nil
+              field :#{prefixed_name}, :type => Hash, :default => {}
             CALLBACK
 
             define_fields_method
